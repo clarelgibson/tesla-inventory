6f6a361dce40fa8e12abe281fe3d699ce0514b74
Clare Gibson <clarelgibson@gmail.com>
2023-03-26 18:06:10 +0100

    Adds stringr package

--- getData.R
+++ getData.R
@@ -14,6 +14,7 @@ library(dplyr)
 library(readr)
 library(janitor)
 library(glue)
+library(stringr)
 
 # GET DATA FROM API ############################################################
 api <- "https://www.tesla.com/inventory/api/v1/inventory-results"
